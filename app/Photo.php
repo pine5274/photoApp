86a602c3099e2d46f9c86f5e5f9fa5f92a79ce01
pine5274 <pine5274@gmail.com>
2020-09-11 16:21:06 +0900

    ch15

--- app/Photo.php
+++ app/Photo.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Auth;
 
 
 class Photo extends Model
@@ -12,20 +13,21 @@ class Photo extends Model
     /** プライマリキーの型 */
     protected $keyType = 'string';
 
+    /** JSONに含めるアクセサ */
+    protected $appends = [
+        'url', 'likes_count', 'liked_by_user',
+    ];
+
     /** JSONに含める属性 */
     protected $visible = [
         'id', 'owner', 'url', 'comments',
+        'likes_count', 'liked_by_user',
     ];
 
     /** JSONに含めない属性 */
     protected $hidden = [
         'user_id', 'filename',
         self::CREATED_AT, self::UPDATED_AT,
-    ];
-
-    /** JSONに含める属性 */
-    protected $appends = [
-        'url',
     ];
 
     protected $perPage = 3;
@@ -61,12 +63,45 @@ class Photo extends Model
     }
 
     /**
+     * アクセサ - likes_count
+     * @return int
+     */
+    public function getLikesCountAttribute()
+    {
+        return $this->likes->count();
+    }
+
+    /**
+     * アクセサ - liked_by_user
+     * @return boolean
+     */
+    public function getLikedByUserAttribute()
+    {
+        if (Auth::guest()) {
+            return false;
+        }
+
+        return $this->likes->contains(function ($user) {
+            return $user->id === Auth::user()->id;
+        });
+    }
+
+    /**
      * リレーションシップ - commentsテーブル
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
     public function comments()
     {
         return $this->hasMany('App\Comment')->orderBy('id', 'desc');
+    }
+
+    /**
+     * リレーションシップ - usersテーブル
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function likes()
+    {
+        return $this->belongsToMany('App\User', 'likes')->withTimestamps();
     }
 
     /**
